cf1d4411da9ffb61c9fe090bc7d7aed0b3212a81
syhuyit <nguyensyhuy06122005@gmail.com>
2026-05-14 22:39:20 +0700

    create API

--- src/config/api.js
+++ src/config/api.js
@@ -3,14 +3,12 @@ import axios from "axios";
 const API = axios.create({
   baseURL: "http://localhost:9999",
 });
-//lay bang Category
-export const getCategories = () => API.get("/Category");
 //lay bang Product
-export const getProducts = () => API.get("/Product");
-export const getProductById = (id) => API.get(`/Product/${id}`);
+export const getProducts = () => API.get("/products");
+export const getProductById = (id) => API.get(`/products/${id}`);
 //them 1 san pham vao Product
-export const addProduct = (data) => API.post(`/Product`, data);
+export const addProduct = (data) => API.post(`/products`, data);
 //xoa 1 san pham khoi Product
-export const deleteProduct = (id) => API.delete(`/Product/${id}`);
+export const deleteProduct = (id) => API.delete(`/products/${id}`);
 // sua 1 san pham
-export const updateProduct = (id, data) => API.put(`/Product/${id}`, data);
+export const updateProduct = (id, data) => API.put(`/products/${id}`, data);
