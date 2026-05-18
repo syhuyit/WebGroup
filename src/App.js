7919335a4f0ae47638d6697d4fe154266f4bfda1
syhuyit <nguyensyhuy06122005@gmail.com>
2026-05-18 10:53:09 +0700

    `Added authentication system with login and register pages, updated routes and context`

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Container } from "react-bootstrap";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
 import Home from "./pages/userPages/Home";
@@ -8,21 +7,77 @@ import AddProduct from "./pages/adminPages/AddProduct";
 import UpdateProduct from "./pages/adminPages/UpdateProduct";
 import Dashboard from "./pages/adminPages/Dashboard";
 
+import { useContext } from "react";
+import { AuthContext } from "./context/AuthContext";
+import Login from "./pages/Login";
+import ProtectedRoute from "./routes/ProtectedRoute";
+import Register from "./pages/Register";
+
+function AppContent() {
+  const { user } = useContext(AuthContext);
+  const location = useLocation();
+
+  const hideNavbar =
+    location.pathname === "/login" ||
+    location.pathname === "/register" ||
+    user?.role === "admin";
+
+  return (
+    <>
+      {!hideNavbar && <Navbar />}
+
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/menu"
+          element={
+            <ProtectedRoute>
+              <Menu />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/add"
+          element={
+            <ProtectedRoute>
+              <AddProduct />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/update/:id"
+          element={
+            <ProtectedRoute>
+              <UpdateProduct />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </>
+  );
+}
+
 function App() {
   return (
     <BrowserRouter>
-      <Navbar />
-      <Container>
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-
-          <Route path="/home" element={<Home />} />
-          <Route path="/menu" element={<Menu />} />
-
-          <Route path="/add" element={<AddProduct />} />
-          <Route path="/update/:id" element={<UpdateProduct />} />
-        </Routes>
-      </Container>
+      <AppContent />
     </BrowserRouter>
   );
 }
--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -2,4 +2,26 @@ import { createContext, useState } from "react";
 
 export const AuthContext = createContext();
 
-function AuthProvider({ children }) {}
+function AuthProvider({ children }) {
+  const [user, setUser] = useState(null);
+
+  const login = (userData) => {
+    setUser(userData);
+  };
+  const logout = () => {
+    setUser(null);
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{
+        user,
+        login,
+        logout,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+}
+export default AuthProvider;
